5de239782faba54c28454a535c7dc9720c72a17f
kavli <lzylzu@qq.com>
2015-10-26 11:03:29 +0800

    new

---
+++ bluetooth_test/src/com/maxfun/bluetooth_test/BluetoothTest.java
@@ -0,0 +1,15 @@
+package com.maxfun.bluetooth_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BluetoothTest extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
